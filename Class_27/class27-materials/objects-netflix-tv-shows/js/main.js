9887b478e2a1b7e91132312f83e9c8d013eaaddc
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-06-06 20:18:41 -0400

    Added class 27 exercises

--- Class_27/class27-materials/objects-netflix-tv-shows/js/main.js
+++ Class_27/class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,21 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
+class NetflixShows{
+    constructor(showTitle,showGenre,showLength,showRating){
+        this.title = showTitle
+        this.genre = showGenre
+        this.length = showLength
+        this.rating = showRating
+    }
+    play(){
+        alert(`${this.title} is starting`)
+    }
+    pause(){
+        alert(`${this.title} is paused`)
+    }
+    stop(){
+        alert(`Stopped watching the ${this.title}`)
+    }
+}
 
+let StrangerThings = new NetflixShows('Stranger Things', 'Fantasy', 45, '99%')
