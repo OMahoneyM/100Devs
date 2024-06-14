40736bd0fca48efb091513fbb7b1d790b6ae30f5
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-06-14 15:50:25 -0400

    Added exercise answers

--- Class_28/class28-materials/dnd/js/main.js
+++ Class_28/class28-materials/dnd/js/main.js
@@ -8,7 +8,27 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       
+       console.log(data)
+       document.querySelector('h2').innerText = data.name
+       document.querySelector('h3').innerText = `Class: ${data.classes[0].name}`
+
+       //remove any subclass li between searches
+       let listElements = document.querySelectorAll('li');
+
+       for (let i = 0; (li = listElements[i]); i++) {
+            li.parentNode.removeChild(li);
+       }
+
+       data.subclasses.forEach(e => {
+        // create an li
+        const li = document.createElement('li')
+        // add text to li
+        li.textContent = e.name
+        // append the li to the ul
+        document.querySelector('ul').appendChild(li)
+       })
+
+
       })
       .catch(err => {
           console.log(`error ${err}`)
