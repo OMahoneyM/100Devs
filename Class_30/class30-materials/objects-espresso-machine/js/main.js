7348ef57720ec1c6789cf7e65b4aa94bd0ffaa59
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-06-21 12:46:41 -0400

    Finished class 30 exercises

--- Class_30/class30-materials/objects-espresso-machine/js/main.js
+++ Class_30/class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,39 @@
-//Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+//Create an espresso machine class that makes machines with 4 properties and 3 methods
+
+class EspressoMachine{
+    constructor(cups, brand, color){
+        this.cups = cups
+        this.brand = brand
+        this.color = color
+        this._power = false
+    }
+    brew(){
+        console.log(`Brewing ${this.cups} cups of espresso`)
+    }
+    steam(){
+        console.log('Steaming the milk')
+    }
+    get power(){
+        if(typeof this._power === 'boolean'){
+            this._power ? console.log('Machine is on.') : console.log('Machine is off.')
+        } else{
+            console.log('The machine is on the fritz.')
+        }
+    }
+    set power(bool){
+        if(typeof bool === 'boolean'){
+            this._power = bool
+        } else {
+            console.log('Please enter a Boolean')
+        }
+    }
+}
+
+let delonghi = new EspressoMachine(4, 'Delonghi', 'silver')
+
+delonghi.brew()
+delonghi.steam()
+delonghi.power
+delonghi.power = 'on'
+delonghi.power = true
+delonghi.power
