e35454dda9d649d9652b16d48da874718222f744
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-06-21 15:26:09 -0400

    Completed exercise

--- Class_31/class31-materials/objects-child-class/js/main.js
+++ Class_31/class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,35 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Animal{
+    constructor(name){
+        this._name = name
+    }
+    get name(){
+        return this._name
+    }
+    speak(){
+        console.log(`${this._name} makes a sound`)
+    }
+}
+
+class Bat extends Animal{
+    constructor(name, species){
+        super(name)
+        this._species = species
+    }
+
+    get species(){
+        return this._species
+    }
+
+    speak(){
+        super.speak()
+        console.log(`${this._name} screeches`)
+    }
+}
+
+let lazlo = new Bat('Lazlo', 'vampire')
+
+lazlo.speak()
+console.log(lazlo.name)
+console.log(lazlo.species)
